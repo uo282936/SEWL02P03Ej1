4d8ed5798facacc4e9fa83271f81d8c9a1b0da21
uo282936 <113896506+uo282936@users.noreply.github.com>
2022-11-22 17:49:08 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y Estándares para la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2022-2023";
+asignatura.estudiante = "Alex Rodriguez Valdes";
+asignatura.email = "uo282936@uniovi.es"
